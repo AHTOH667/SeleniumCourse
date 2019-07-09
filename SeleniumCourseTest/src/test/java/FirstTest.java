903c8d14d6f67f6ae125ac2613c528c7b498e34c
AHTOH <apoddenezh@wiley.com>
2019-07-09 12:12:20 +0300

    sdd selenium

--- SeleniumCourseTest/src/test/java/FirstTest.java
+++ SeleniumCourseTest/src/test/java/FirstTest.java
@@ -1,11 +1,22 @@
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class FirstTest {
 
     public static void main(String[] args) {
 
-        ChromeDriver driver = new ChromeDriver();
+        WebDriver driver = new ChromeDriver();
         driver.get("https://www.google.com/");
         driver.quit();
+
+        WebDriver driver2 = new FirefoxDriver();
+        driver2.get("https://www.google.com/");
+        driver2.quit();
+
+        WebDriver driver3 = new InternetExplorerDriver();
+        driver3.get("https://www.google.com/");
+        driver3.quit();
     }
 }
--- SeleniumCourseTest/src/test/java/LoginTest.java
+++ SeleniumCourseTest/src/test/java/LoginTest.java
@@ -1,11 +1,13 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class LoginTest {
 
     public static void main(String[] args) {
 
-        ChromeDriver driver = new ChromeDriver();
+        WebDriver driver = new FirefoxDriver();
         driver.get("http://localhost/litecart/admin/login.php");
         driver.findElement(By.name("username")).sendKeys("admin");
         driver.findElement(By.name("password")).sendKeys("admin");
