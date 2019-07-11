902bf2c27be21655907b3873f2ca45ff36452b15
AHTOH <apoddenezh@wiley.com>
2019-07-11 11:00:20 +0300

    Add test Section Of Admin

--- SeleniumCourseTest/src/test/java/LoginTest.java
+++ SeleniumCourseTest/src/test/java/LoginTest.java
@@ -1,36 +1,27 @@
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxBinary;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxOptions;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-import java.io.File;
 
 
 public class LoginTest {
 
 
-    public static WebDriver driver;
+    private TestBase test = new TestBase();
 
     @BeforeMethod
-    public void start() {
-        if (driver != null) {
-            return;
-        }
-        FirefoxOptions options = new FirefoxOptions();
-        options.setBinary(new FirefoxBinary(new File("C:\\Program Files\\Firefox Nightly\\firefox.exe")));
-        driver = new FirefoxDriver(options);
-        driver.get("http://localhost/litecart/admin/login.php");
+    public void before() {
+        test.start();
     }
 
 
     @Test
     public void login() {
+        test.login();
+    }
 
-        driver.findElement(By.name("username")).sendKeys("admin");
-        driver.findElement(By.name("password")).sendKeys("admin");
-        driver.findElement(By.name("login")).click();
-        driver.quit();
+
+    @AfterMethod
+    public void after() {
+        test.quit();
     }
 }
--- SeleniumCourseTest/src/test/java/TestBase.java
+++ SeleniumCourseTest/src/test/java/TestBase.java
@@ -1,9 +1,17 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+
 public class TestBase {
 
+
+    public static WebDriver getDriver() {
+        return driver;
+    }
+
     public static WebDriver driver;
+    
 
     public void start() {
         if (driver != null) {
@@ -12,5 +20,21 @@ public class TestBase {
         driver = new ChromeDriver();
         driver.get("http://localhost/litecart/admin/login.php");
 
+    }
+
+    public void login() {
+        driver.findElement(By.name("username")).sendKeys("admin");
+        driver.findElement(By.name("password")).sendKeys("admin");
+        driver.findElement(By.name("login")).click();
+    }
+
+
+
+    public void clickOnLeftRail(By locator) {
+        driver.findElement(locator).click();
+    }
+
+    public void quit() {
+        driver.quit();
     }
 }
